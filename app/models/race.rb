4f05532c6cc2470f80d03bd616c7f96603262f85
Aldar Antonov <aantonov84@gmail.com>
2016-02-13 18:52:45 +0300

    add past and upcoming scopes in race model

--- app/models/race.rb
+++ app/models/race.rb
@@ -4,6 +4,9 @@ class Race
 
   embeds_many :events, as: :parent, class_name: 'Event', order: [:order.asc]
 
+  scope :upcoming, ->{ where(:date.gte => Date.current) }
+  scope :past, ->{ where(:date.lt => Date.current) }
+
   field :n, as: :name, type: String
   field :date, as: :date, type: Date
   field :loc, as: :location, type: Address
