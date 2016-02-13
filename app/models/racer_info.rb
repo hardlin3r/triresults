b1d897850ba07c426fc741d1e030c48ea8e04529
Aldar Antonov <aantonov84@gmail.com>
2016-02-13 17:54:46 +0300

    add racer info validations

--- app/models/racer_info.rb
+++ app/models/racer_info.rb
@@ -1,5 +1,10 @@
 class RacerInfo
   include Mongoid::Document
+
+  validates :first_name, :last_name, :gender, :birth_year, presence: true
+  validates :gender, inclusion: { in: ["M", "F"] }
+  validates :birth_year, numericality: { only_integer: true, less_than: Time.now.year }
+
   embedded_in :parent, polymorphic: true
   field :racer_id, as: :_id
   field :_id, default: ->{ racer_id }
