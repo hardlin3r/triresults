b1942f65bfbc918aae71c549b9f487c59ef5cb4f
Aldar Antonov <aantonov84@gmail.com>
2016-02-13 18:18:26 +0300

    add event model

---
+++ app/models/event.rb
@@ -0,0 +1,37 @@
+class Event
+  include Mongoid::Document
+  field :o, as: :order, type: Integer
+  field :n, as: :name, type: String
+  field :d, as: :distance, type: Float
+  field :u, as: :units, type: String
+
+  def meters
+    case u
+    when "miles" then
+      d / 0.000621371
+    when "yards" then
+      d * 0.9144
+    when "kilometers" then
+      d * 1000
+    when "meters" then
+      d
+    else
+      nil
+    end
+  end
+
+  def miles
+    case u
+    when "meters" then
+      d * 0.000621371
+    when "yards" then
+      d * 0.000568182
+    when "kilometers" then
+      d * 0.621371
+    when "miles" then
+      d
+    else
+      nil
+    end
+ end
+end
