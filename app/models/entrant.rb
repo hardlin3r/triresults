e3d8d8def8fb3c5c7590e58f7ccc6247e3cb74fc
Aldar Antonov <aantonov84@gmail.com>
2016-02-14 04:27:42 +0300

    add 1:1 embedded relationship betweet Entrant and RacerInfo

--- app/models/entrant.rb
+++ app/models/entrant.rb
@@ -4,6 +4,7 @@ class Entrant
 
   embeds_many :results, class_name: 'LegResult', order: [:"event.o".asc], after_add: :update_total
   embeds_one :race, class_name: 'RaceRef'
+  embeds_one :racer, as: :parent, class_name: "RacerInfo"
 
   store_in collection: "results"
 
