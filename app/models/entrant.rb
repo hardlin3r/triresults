0e807306b224ed3044079deaabfc435008282ca0
Aldar Antonov <aantonov84@gmail.com>
2016-02-15 00:36:55 +0300

    add delegation to Entrant model

--- app/models/entrant.rb
+++ app/models/entrant.rb
@@ -3,8 +3,8 @@ class Entrant
   include Mongoid::Timestamps
 
   embeds_many :results, class_name: 'LegResult', order: [:"event.o".asc], after_add: :update_total
-  embeds_one :race, class_name: 'RaceRef'
-  embeds_one :racer, as: :parent, class_name: "RacerInfo"
+  embeds_one :race, class_name: 'RaceRef', autobuild: true
+  embeds_one :racer, as: :parent, class_name: "RacerInfo", autobuild: true
 
   store_in collection: "results"
 
@@ -13,6 +13,29 @@ class Entrant
   field :o, as: :overall, type: Placing
   field :gender, type: Placing
   field :group, type: Placing
+
+  delegate :first_name, :first_name=, to: :racer
+  delegate :last_name, :last_name=, to: :racer
+  delegate :gender, :gender=, to: :racer, prefix: "racer"
+  delegate :birth_year, :birth_year=, to: :racer
+  delegate :city, :city=, to: :racer
+  delegate :state, :state=, to: :racer
+
+  delegate :name, :name=, to: :race, prefix: "race"
+  delegate :date, :date=, to: :race, prefix: "race"
+
+  def overall_place
+    overall.place if overall
+  end
+  def gender_place
+    gender.place if gender
+  end
+  def group_name
+    group.name if group
+  end
+  def group_place
+    group.place if group
+  end
 
   def update_total(result)
     self.secs = 0.0 if self.secs.nil?
