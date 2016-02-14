9c56bde89436e1c3947718d1765dff886d1ae443
Aldar Antonov <aantonov84@gmail.com>
2016-02-14 04:34:27 +0300

    add 1:M relationship between Racer and Entrant

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -1,5 +1,7 @@
 class Racer
   include Mongoid::Document
+  has_many :races, class_name: "Entrant", foreign_key: "racer.racer_id", dependent: :nullify, order: :"race.date".desc
+
   before_create do |racer|
     racer.info.id = racer.id
   end
