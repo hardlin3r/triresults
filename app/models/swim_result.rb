898c5e1541af6ee455e2971c6e0220f038cc85e5
Aldar Antonov <aantonov84@gmail.com>
2016-02-15 14:16:34 +0300

    correct swim_results#calc_ave

--- app/models/swim_result.rb
+++ app/models/swim_result.rb
@@ -5,7 +5,7 @@ class SwimResult < LegResult
   def calc_ave
     if event && secs
       meters = event.meters
-      self.pace_100 = meters.nil? ? nil: meters / self.secs
+      self.pace_100 = meters.nil? ? nil: self.secs / (meters / 100)
     end
   end
 
